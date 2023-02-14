bf436b10b23e1d08f9674990b2b1631dc7fb67c7
Meagan Lang <langmm.astro@gmail.com>
2023-02-14 18:40:16 -0500

    Hard code min/max values for specific precision integers to allow for use with MSVC that seems to be missing them on GHA image

--- include/rapidjson/precision.h
+++ include/rapidjson/precision.h
@@ -11,6 +11,48 @@
 #include <cstdint>
 #endif
 
+// 8 bit
+#ifndef INT8_MAX
+#define INT8_MAX 127
+#endif // INT8_MAX
+#ifndef INT8_MIN
+#define INT8_MIN -128
+#endif // INT8_MIN
+#ifndef UINT8_MAX
+#define UINT8_MAX 255
+#endif // UINT8_MAX
+// 16 bit
+#ifndef INT16_MAX
+#define INT16_MAX 32767
+#endif // INT16_MAX
+#ifndef INT16_MIN
+#define INT16_MIN −32768
+#endif // INT16_MIN
+#ifndef UINT16_MAX
+#define UINT16_MAX 65535
+#endif // UINT16_MAX
+// 32 bit
+#ifndef INT32_MAX
+#define INT32_MAX 2147483647
+#endif // INT32_MAX
+#ifndef INT32_MIN
+#define INT32_MIN -2147483648
+#endif // INT32_MIN
+#ifndef UINT32_MAX
+#define UINT32_MAX 4294967295
+#endif // UINT32_MAX
+// 64 bit
+#ifndef INT64_MAX
+#define INT64_MAX 9223372036854775807
+#endif // INT64_MAX
+#ifndef INT64_MIN
+#define INT64_MIN -9223372036854775808
+#endif // INT64_MIN
+#ifndef UINT64_MAX
+#define UINT64_MAX 18446744073709551615
+#endif // UINT64_MAX
+
+
 RAPIDJSON_NAMESPACE_BEGIN
 
 #ifdef RAPIDJSON_YGGDRASIL
