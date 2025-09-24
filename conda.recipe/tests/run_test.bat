4198e14f70adc2faa27a0655186fa5b4a0c8ccd6
Meagan Lang <langmm.astro@gmail.com>
2025-09-24 12:41:42 -0400

    Added variants file for stdlib and re-enable stdlib dependency Added test requirements to recipes & combined test in v1 version for unix/win Try to fix bugs in batch run_tests

--- conda.recipe/tests/run_test.bat
+++ conda.recipe/tests/run_test.bat
@@ -1,22 +1,26 @@
 @setlocal
+setlocal EnableDelayedExpansion
 
 set examples=serialize yggdrasil units
 
-(for %%example in (%examples%) do (
-     echo Building %%example
-     if not exist "example\%%example\build" mkdir example\%%example\build
-     cd example\%%example\build
-     cmake -G "Ninja" ^
-           -D CMAKE_VERBOSE_MAKEFILE:BOOL=ON ^
-           ..
-     if errorlevel 1 exit 1
-     cmake --build . --config Debug
-     if errorlevel 1 exit 1
+for %%example in (%examples%) do (
+  echo "Building %%example"
+  set builddir="example\%%example\build"
+  if not exist "!builddir!" mkdir "!builddir!"
+  if !errorlevel! neq 0 exit /b !errorlevel!
+  cd "!builddir!"
+  cmake -G "Ninja" ^
+        -D CMAKE_VERBOSE_MAKEFILE:BOOL=ON ^
+        ..
+  if !errorlevel! neq 0 exit /b !errorlevel!
+  cmake --build . --config Debug
+  if !errorlevel! neq 0 exit /b !errorlevel!
 
-     echo Running %%example
-     %%example.exe
-     if errorlevel 1 exit 1
+  echo "Running %%example"
+  "%%example.exe"
+  if !errorlevel! neq 0 exit /b !errorlevel!
 
-     cd ..\..\..
-))
+  cd ..\..\..
+)
+
 @endlocal
