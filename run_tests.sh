f065bdbb137981525233ebea03ecafa76f84e660
Meagan Lang <langmm.astro@gmail.com>
2025-11-11 14:07:35 -0500

    Update names used in build system

--- run_tests.sh
+++ run_tests.sh
@@ -8,19 +8,24 @@ if [ ! -d ${INSTALL_DIR} ]; then
     mkdir ${INSTALL_DIR}
 fi
 cd build
-# cmake .. -DRAPIDJSON_HAS_STDSTRING:BOOL=ON -DRAPIDJSON_BUILD_TESTS:BOOL=OFF -DRAPIDJSON_BUILD_EXAMPLES:BOOL=OFF -DRAPIDJSON_BUILD_DOC:BOOL=OFF -DCMAKE_INSTALL_PREFIX:FILEPATH=${INSTALL_DIR}
-# cmake --install . --prefix "${INSTALL_DIR}"
-cmake .. -DRAPIDJSON_SKIP_VALGRIND_TESTS=ON -DRAPIDJSON_ENABLE_COVERAGE=OFF -DCMAKE_BUILD_TYPE=Debug -DRAPIDJSON_CREATE_METASCHEMA_FULL=ON -DRAPIDJSON_YGGDRASIL_TESTS=ON -DRAPIDJSON_BUILD_UBSAN=ON -DRAPIDJSON_BUILD_ASAN=ON
-# cmake --build . -- -j 8
+
+cmake .. -DYGGDRASIL_RAPIDJSON_HAS_STDSTRING:BOOL=ON -DYGGDRASIL_RAPIDJSON_BUILD_TESTS:BOOL=ON -DYGGDRASIL_RAPIDJSON_BUILD_EXAMPLES:BOOL=OFF -DYGGDRASIL_RAPIDJSON_BUILD_DOC:BOOL=OFF
 cmake --build . --target=tests -- -j 8
-# ctest -C Debug --output-on-failure --verbose --stop-on-failure
 ctest -R unittest --stop-on-failure
-# ctest -R coverage
-# ctest -R coverage
-# cmake .. -DRAPIDJSON_SKIP_VALGRIND_TESTS=ON -DRAPIDJSON_ENABLE_COVERAGE=ON -DCMAKE_BUILD_TYPE=Debug
-# ctest -T Coverage
-# ./bin/unittest
-# export DATADIR=/Users/langmm/rapidjson/test
-# export YGG_PYTHON_EXEC=/Users/langmm/miniconda3/envs/conda37/bin/python
-# valgrind --leak-check=full   --show-leak-kinds=all --dsymutil=no --track-origins=yes -v --suppressions=/Users/langmm/valgrind-macos/darwin13.supp ./bin/unittest &> log.txt
-# --suppressions=/Users/langmm/valgrind-macos/default.supp ./bin/unittest &> log.txt
+cmake --install .
+# cmake .. -DYGGDRASIL_RAPIDJSON_HAS_STDSTRING:BOOL=ON -DYGGDRASIL_RAPIDJSON_BUILD_TESTS:BOOL=OFF -DYGGDRASIL_RAPIDJSON_BUILD_EXAMPLES:BOOL=OFF -DYGGDRASIL_RAPIDJSON_BUILD_DOC:BOOL=OFF -DCMAKE_INSTALL_PREFIX:FILEPATH=${INSTALL_DIR}
+# cmake --install . --prefix "${INSTALL_DIR}"
+# cmake .. -DYGGDRASIL_RAPIDJSON_SKIP_VALGRIND_TESTS=ON -DYGGDRASIL_RAPIDJSON_ENABLE_COVERAGE=OFF -DCMAKE_BUILD_TYPE=Debug -DYGGDRASIL_RAPIDJSON_CREATE_METASCHEMA_FULL=ON -DYGGDRASIL_RAPIDJSON_TESTS=ON -DYGGDRASIL_RAPIDJSON_BUILD_UBSAN=ON -DYGGDRASIL_RAPIDJSON_BUILD_ASAN=ON
+# # cmake --build . -- -j 8
+# cmake --build . --target=tests -- -j 8
+# # ctest -C Debug --output-on-failure --verbose --stop-on-failure
+# ctest -R unittest --stop-on-failure
+# # ctest -R coverage
+# # ctest -R coverage
+# # cmake .. -DYGGDRASIL_RAPIDJSON_SKIP_VALGRIND_TESTS=ON -DYGGDRASIL_RAPIDJSON_ENABLE_COVERAGE=ON -DCMAKE_BUILD_TYPE=Debug
+# # ctest -T Coverage
+# # ./bin/unittest
+# # export DATADIR=/Users/langmm/rapidjson/test
+# # export YGG_PYTHON_EXEC=/Users/langmm/miniconda3/envs/conda37/bin/python
+# # valgrind --leak-check=full   --show-leak-kinds=all --dsymutil=no --track-origins=yes -v --suppressions=/Users/langmm/valgrind-macos/darwin13.supp ./bin/unittest &> log.txt
+# # --suppressions=/Users/langmm/valgrind-macos/default.supp ./bin/unittest &> log.txt
