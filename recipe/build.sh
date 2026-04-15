5cc807757c70a6b4905c5f58ad1b4cb87adf967f
Meagan Lang <langmm.astro@gmail.com>
2026-04-15 14:50:14 -0400

    Revert changes to handling of Python/Numpy include directories in packaging and update recipes

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,13 +1,19 @@
 #!/bin/sh
 set -euo pipefail
 
+Python_INCLUDE_DIR="$(python -c 'import sysconfig; print(sysconfig.get_path("include"))')"
+Python_NumPy_INCLUDE_DIR="$(python -c 'import numpy; print(numpy.get_include())')"
+
+CMAKE_ARGS+=" -DPython3_EXECUTABLE:PATH=${PYTHON}"
+CMAKE_ARGS+=" -DPython3_INCLUDE_DIR:PATH=${Python_INCLUDE_DIR}"
+CMAKE_ARGS+=" -DPython3_NumPy_INCLUDE_DIR=${Python_NumPy_INCLUDE_DIR}"
+
 cmake -B build -S ${SRC_DIR} \
       -G "Ninja" \
       -D YGGDRASIL_RAPIDJSON_HAS_STDSTRING:BOOL=ON \
       -D YGGDRASIL_RAPIDJSON_BUILD_TESTS:BOOL=OFF \
       -D YGGDRASIL_RAPIDJSON_BUILD_EXAMPLES:BOOL=OFF \
       -D YGGDRASIL_RAPIDJSON_BUILD_DOC:BOOL=OFF \
-      -D Python3_EXECUTABLE=$PYTHON \
       -D CMAKE_VERBOSE_MAKEFILE:BOOL=ON \
        ${CMAKE_ARGS}
 cmake --build build -j${CPU_COUNT}
